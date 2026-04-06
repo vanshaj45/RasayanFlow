412ee2c3d84d2499e3b9df65b4c988f73cc79221
Vanshaj Bairagi <vanshajbairagi10@gmail.com>
2026-04-06 12:46:29 +0530

    Deploy latest backend config updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const logger = require('../utils/logger');
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI || ;
+  const uri = process.env.MONGO_URI || 'mongodb+srv://vanshajbairagi10_db_user:JmSP6v9qAMx75VBR@cluster0.qnzs16w.mongodb.net/Pharmlab2' ;
 
   await mongoose.connect(uri, {
     useNewUrlParser: true,
